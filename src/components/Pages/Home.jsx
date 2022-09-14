ad0a48cd6bf46f5a2f36fc84559e011a8bbe650f
Franco Martin Paturlanne <paturlannefm@gmail.com>
2022-09-14 17:09:53 -0300

    Learning grid layout

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react"
 import Banner from "../Organisms/Banner"
+import "./learngrid.css"
 
 const Home = ( ) => (
-   <Banner/>
+   <div>
+      <Banner/>
+      <div className="grid-container">
+         <div className="item1">Header</div>
+         <div className="item2">Menu</div>
+         <div className="item3">Main</div>
+         <div className="item4">Aside</div>
+         <div className="item5">Footer</div>
+      </div>
+   </div>
+   
 )
 
 
